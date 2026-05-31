76791fc912406061c2a675a0813d1277f0f17955
Karl Crisostomo <karlvincent.crisostomo@gmail.com>
2026-05-31 21:20:03 +0800

    Create first files

---
+++ FinancialMatrix/app/src/main/java/com/karlvcrisostomo/financialmatrix/data/TransactionEntity.kt
@@ -0,0 +1,16 @@
+package com.yourname.financialmatrix.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "transactions")
+data class TransactionEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val description: String,
+    val amount: Double,
+    val date: LocalDate,
+    val category: String,
+    val isCreditCard: Boolean,
+    val accountName: String
+)
